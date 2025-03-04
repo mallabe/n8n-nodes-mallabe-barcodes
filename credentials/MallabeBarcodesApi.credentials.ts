4dd1dab46234818c18224c56d0c674d38a3a2853
Din Bracha <din.bracha@autodesk.com>
2025-03-04 23:45:18 +0200

    send user-agent

--- credentials/MallabeBarcodesApi.credentials.ts
+++ credentials/MallabeBarcodesApi.credentials.ts
@@ -23,6 +23,7 @@ export class MallabeBarcodesApi implements ICredentialType {
 		type: 'generic',
 		properties: {
 			headers: {
+				'User-Agent': 'n8n',
 				'x-api-key': N8N_API_KEY,
 				'x-account-api-key': '={{$credentials.apiKey}}'
 			},
